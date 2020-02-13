581e429a82fd9dd3163b9066d3fb8e96e959b718
Michael Cerase <michaelcerase@gmail.com>
2020-02-13 10:39:26 -0600

    Created GetAllDepts

--- BestBuyIntro/Program.cs
+++ BestBuyIntro/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace BestBuyIntro
 {
@@ -6,7 +9,39 @@ namespace BestBuyIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var depart = GetAllDepartments();
+
+            foreach(var dept in depart)
+            {
+                Console.WriteLine(dept);
+            }
+        }
+
+        public static IEnumerable GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT name FROM departments";
+
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("name");
+                    allDepartments.Add(currentDepartment);
+                }
+                return allDepartments;
+            }
+
+
         }
     }
 }
