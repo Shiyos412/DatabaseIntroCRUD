3c42594ef12d800a1e468971a6ca9ae2712e5419
Michael Cerase <michaelcerase@gmail.com>
2020-02-13 13:45:35 -0600

    Added Department class for easier handling of info

---
+++ BestBuyIntro/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace BestBuyIntro
+{
+    internal class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+
+        public Department(int currentDepartmentID, string currentDepartmentName)
+        {
+            DepartmentID = currentDepartmentID;
+            DepartmentName = currentDepartmentName;
+        }
+
+        public Department() { }
+    }
+}
--- BestBuyIntro/Program.cs
+++ BestBuyIntro/Program.cs
@@ -11,37 +11,147 @@ namespace BestBuyIntro
         {
             var depart = GetAllDepartments();
 
+            //var userInput = Console.ReadLine();
+
+            //CreateDepartment(userInput);
+
+            depart = GetAllDepartments();
+
             foreach(var dept in depart)
             {
-                Console.WriteLine(dept);
+                
+                Console.WriteLine($"{dept.DepartmentID}: {dept.DepartmentName}");
+            }
+
+            Console.WriteLine("Do you want to update a Department?");
+            string input = Console.ReadLine();
+            bool may = (input.ToLower() == "yes" ? true : false);
+            while (may)
+            {
+                Console.WriteLine("Which department do you want to update? Type the number:");
+                int deptID = int.Parse(Console.ReadLine());
+                Console.WriteLine("What will the new name be?");
+                string newName = Console.ReadLine();
+                UpdateDepartment(deptID, newName);
+                Console.WriteLine("Type yes to update another:");
+                may = Console.ReadLine().ToLower() == "yes" ? true : false;
+            }
+            depart = GetAllDepartments();
+
+            foreach(var dept in depart)
+            {
+                Console.WriteLine($"{dept.DepartmentID}: {dept.DepartmentName}");
             }
         }
 
-        public static IEnumerable GetAllDepartments()
+        static Department FindDepartment(int ID)
         {
             MySqlConnection conn = new MySqlConnection();
             conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
 
             MySqlCommand cmd = conn.CreateCommand();
 
-            cmd.CommandText = "SELECT name FROM departments";
-
+            cmd.CommandText = "SELECT departmentid, name FROM departments where departmentid = @ID";
+            cmd.Parameters.AddWithValue("ID", ID);
             using (conn)
             {
                 conn.Open();
-                List<string> allDepartments = new List<string>();
+                Department myDepartment = new Department();
 
                 MySqlDataReader reader = cmd.ExecuteReader();
 
                 while(reader.Read() == true)
                 {
-                    var currentDepartment = reader.GetString("name");
+                    myDepartment.DepartmentID = reader.GetInt32("departmentid");
+                    myDepartment.DepartmentName = reader.GetString("name");
+                }
+                return myDepartment;
+            }
+        }
+
+        public static List<Department> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            MySqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = "SELECT departmentid, name FROM departments";
+
+            using (conn)
+            {
+                conn.Open();
+                List<Department> allDepartments = new List<Department>();
+
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while(reader.Read() == true)
+                {
+                    var currentDepartment = new Department();
+                    currentDepartment.DepartmentID = reader.GetInt32("departmentid");
+                    currentDepartment.DepartmentName = reader.GetString("name");
                     allDepartments.Add(currentDepartment);
+
+
                 }
                 return allDepartments;
             }
 
 
         }
+
+        static void CreateDepartment(string departmentName)
+        {
+            var connStr = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            //If you adopt initializing the connection inside the using statement then you can't make a mistake //later when reorganizing or refactoring code and accidentally doing something that implicitly //opens a connection that isn't closed
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                // parameterized query to prevent SQL Injection
+                cmd.CommandText = "INSERT INTO departments (Name) VALUES (@departmentName);";
+                cmd.Parameters.AddWithValue("departmentName", departmentName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        static void DeleteDepartment(string departmentName)
+        {
+            var connStr = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            using(var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "DELETE FROM departments WHERE name = @departmentName;";
+                cmd.Parameters.AddWithValue("departmentName", departmentName);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        
+
+        static void UpdateDepartment(int departmentID, string newName)
+        {
+            Department updated = new Department();
+            updated.DepartmentID = departmentID;
+            updated.DepartmentName = newName;
+            var connStr = System.IO.File.ReadAllText("ConnectionString.txt");
+
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                MySqlCommand cmd = conn.CreateCommand();
+
+                cmd.CommandText = "UPDATE departments SET name = @newName WHERE departmentID = @departmentID";
+                cmd.Parameters.AddWithValue("newName", updated.DepartmentName);
+                cmd.Parameters.AddWithValue("departmentID", updated.DepartmentID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
     }
 }
